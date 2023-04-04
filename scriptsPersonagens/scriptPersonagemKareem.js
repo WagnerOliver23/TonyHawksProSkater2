d8d8a7bd0046f97eb5e5e8e05dc835eb356f84cb
Wagner Oliver23 <devwagneroliver@gmail.com>
2023-04-03 21:30:33 -0300

    Adicionando novos estilos, arquivos de sons, imagens, colocando eventos de JavaScript no arquivo personagens.html, atra´ves dos arquivos que estão na pasta scriptsPersonagens, criando eventos dessa pasta a esse arquivo html.  English:Adding new styles, sound files, images, placing JavaScript events in the characters.html file, through the files that are in the scriptsPersonagens folder, creating events from that folder to that html file

---
+++ scriptsPersonagens/scriptPersonagemKareem.js
@@ -0,0 +1,271 @@
+function Kareem(){
+
+  var nomePersonagem = window.document.getElementById('nomePersonagem')
+  nomePersonagem.innerHTML = '<strong>Kareem Campbell</strong>'
+
+  var nascimento = window.document.getElementById('nascimento')
+  nascimento.innerHTML = '<strong>Nascimento:</strong> 14 de novembro de 1973'
+
+  var imgPersonagem = window.document.getElementById('imgPersonagem')
+  imgPersonagem.innerHTML = "<img src = './imagesPersonagens/kareemCampbell.jpg' width='150'>";
+
+  var idade = window.document.getElementById('idade')
+  idade.innerHTML = '<strong>Idade:</strong> 49 anos (2023)'
+
+  var nacionalidade = window.document.getElementById('nacionalidade')
+  nacionalidade.innerHTML = '<strong>Nacionalidade: Norte Americano'
+
+  var cidade = window.document.getElementById('cidade')
+  cidade.innerHTML = '<strong>Cidade:</strong> Harlem, Nova Iorque - Eua'
+
+  var paisRepresenta = window.document.getElementById('paisRepresenta')
+  paisRepresenta.innerHTML = '<strong>Representante:</strong> Estados Unidos'
+
+  var imgBandeiraRepresenta = window.document.getElementById('imgBandeiraRepresenta')
+  imgBandeiraRepresenta.innerHTML = "<img src = './imagesBandeiraPaises/eua_bandeira.png' width='100'>";
+
+  var modalidade = window.document.getElementById('altura')
+  modalidade.innerHTML = '<strong>Altura:</strong> 1.77 m'
+
+  var modalidade = window.document.getElementById('modalidade')
+  modalidade.innerHTML = '<strong>Modalidade:</strong> Street'
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+  /* Título Flip Tricks*/
+  var flipTricks = window.document.getElementById('flipTricks')
+  flipTricks.innerHTML = 'Flip Tricks<br>'
+  
+  /* Flip Tricks 1 */
+  var nomeFlipsTricks = window.document.getElementById('nomeFlipsTricks')
+  nomeFlipsTricks.innerHTML = '<strong>Pop Shove it</strong>'
+
+  var flipTricksBotoesImg = window.document.getElementById('flipTricksBotoesImg')
+  flipTricksBotoesImg.innerHTML = "<img src='./imagesArrows/seta_cima_laranja.png' width='25'>";
+  flipTricksBotoesImg.innerHTML += "<img src='./imagesBotoesPsOne/botaoQuadrado.png' width='25'>";
+
+  /* Flip Tricks 2 */
+  var nomeFlipsTricks2 = window.document.getElementById('nomeFlipsTricks2')
+  nomeFlipsTricks2.innerHTML = '<strong>Inward HeelFlip</strong>'
+
+  var flipTricksBotoesImg2 = window.document.getElementById('flipTricksBotoesImg2')
+  flipTricksBotoesImg2.innerHTML = "<img src='./imagesArrows/seta_diagonal_cima_direita.png' width='25'>";
+  flipTricksBotoesImg2.innerHTML += "<img src='./imagesBotoesPsOne/botaoQuadrado.png' width='25'>";
+
+  /* Flip Tricks 3 */
+  var nomeFlipsTricks3 = window.document.getElementById('nomeFlipsTricks3')
+  nomeFlipsTricks3.innerHTML = '<strong>HeelFlip</strong>'
+
+  var flipTricksBotoesImg3 = window.document.getElementById('flipTricksBotoesImg3')
+  flipTricksBotoesImg3.innerHTML = "<img src='./imagesArrows/seta_direita_laranja.png' width='25'>";
+  flipTricksBotoesImg3.innerHTML += "<img src='./imagesBotoesPsOne/botaoQuadrado.png' width='25'>";
+
+  /* Flip Tricks 4 */
+  var nomeFlipsTricks4 = window.document.getElementById('nomeFlipsTricks4')
+  nomeFlipsTricks4.innerHTML = '<strong>Varial HeelFlip</strong>'
+
+  var flipTricksBotoesImg4 = window.document.getElementById('flipTricksBotoesImg4')
+  flipTricksBotoesImg4.innerHTML = "<img src='./imagesArrows/seta_diagonal_baixo_direita.png' width='25'>";
+  flipTricksBotoesImg4.innerHTML += "<img src='./imagesBotoesPsOne/botaoQuadrado.png' width='25'>";
+
+  /* Flip Tricks 5 */
+  var nomeFlipsTricks5 = window.document.getElementById('nomeFlipsTricks5')
+  nomeFlipsTricks5.innerHTML = '<strong>FS Shove It</strong>'
+
+  var flipTricksBotoesImg5 = window.document.getElementById('flipTricksBotoesImg5')
+  flipTricksBotoesImg5.innerHTML = "<img src='./imagesArrows/seta_baixo_laranja.png' width='25'>";
+  flipTricksBotoesImg5.innerHTML += "<img src='./imagesBotoesPsOne/botaoQuadrado.png' width='25'>";
+
+  /* Flip Tricks 6 */
+  var nomeFlipsTricks6 = window.document.getElementById('nomeFlipsTricks6')
+  nomeFlipsTricks6.innerHTML = '<strong>Varial KickFlip</strong>'
+
+  var flipTricksBotoesImg6 = window.document.getElementById('flipTricksBotoesImg6')
+  flipTricksBotoesImg6.innerHTML = "<img src='./imagesArrows/seta_diagonal_baixo_esquerda.png' width='25'>";
+  flipTricksBotoesImg6.innerHTML += "<img src='./imagesBotoesPsOne/botaoQuadrado.png' width='25'>";
+
+  /* Flip Tricks 7 */
+  var nomeFlipsTricks7 = window.document.getElementById('nomeFlipsTricks7')
+  nomeFlipsTricks7.innerHTML = '<strong>KickFlip</strong>'
+
+  var flipTricksBotoesImg7 = window.document.getElementById('flipTricksBotoesImg7')
+  flipTricksBotoesImg7.innerHTML = "<img src='./imagesArrows/seta_esquerda_laranja.png' width='25'>";
+  flipTricksBotoesImg7.innerHTML += "<img src='./imagesBotoesPsOne/botaoQuadrado.png' width='25'>";
+
+  /* Flip Tricks 8 */
+  var nomeFlipsTricks8 = window.document.getElementById('nomeFlipsTricks8')
+  nomeFlipsTricks8.innerHTML = '<strong>HardFlip</strong>'
+
+  var flipTricksBotoesImg8 = window.document.getElementById('flipTricksBotoesImg8')
+  flipTricksBotoesImg8.innerHTML = "<img src='./imagesArrows/seta_diagonal_cima_esquerda.png' width='25'>";
+  flipTricksBotoesImg8.innerHTML += "<img src='./imagesBotoesPsOne/botaoQuadrado.png' width='25'>";
+
+  /* Flip Tricks 9 */
+  var nomeFlipsTricks9 = window.document.getElementById('nomeFlipsTricks9')
+  nomeFlipsTricks9.innerHTML = '<strong>360 Shove It</strong>'
+
+  var flipTricksBotoesImg9 = window.document.getElementById('flipTricksBotoesImg9')
+  flipTricksBotoesImg9.innerHTML = "<img src='./imagesArrows/seta_cima_laranja.png' width='25'>";
+  flipTricksBotoesImg9.innerHTML += "<img src='./imagesArrows/seta_cima_laranja.png' width='25'>";
+  flipTricksBotoesImg9.innerHTML += "<img src='./imagesBotoesPsOne/botaoQuadrado.png' width='25'>";
+
+  /* Flip Tricks 10 */
+  var nomeFlipsTricks10 = window.document.getElementById('nomeFlipsTricks10')
+  nomeFlipsTricks10.innerHTML = '<strong>360 Flip</strong>'
+
+  var flipTricksBotoesImg10 = window.document.getElementById('flipTricksBotoesImg10')
+  flipTricksBotoesImg10.innerHTML = "<img src='./imagesArrows/seta_baixo_laranja.png' width='25'>";
+  flipTricksBotoesImg10.innerHTML += "<img src='./imagesArrows/seta_baixo_laranja.png' width='25'>";
+  flipTricksBotoesImg10.innerHTML += "<img src='./imagesBotoesPsOne/botaoQuadrado.png' width='25'>";
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+  /* Grab Tricks */
+  /* Título Grab Tricks*/
+  var grabTricks = window.document.getElementById('grabTricks')
+  grabTricks.innerHTML = 'Grab Tricks<br>'
+
+  /* Grab Tricks 1 */
+  var nomeGrabTricks = window.document.getElementById('nomeGrabTricks')
+  nomeGrabTricks.innerHTML = '<strong>NoseGrab</strong>'
+
+  var grabTricksBotoesImg = window.document.getElementById('grabTricksBotoesImg')
+  grabTricksBotoesImg.innerHTML = "<img src='./imagesArrows/seta_cima_laranja.png' width='25'>";
+  grabTricksBotoesImg.innerHTML += "<img src='./imagesBotoesPsOne/botaoBolinha.png' width='25'>";
+
+  /* Grab Tricks 2 */
+  var nomeGrabTricks2 = window.document.getElementById('nomeGrabTricks2')
+  nomeGrabTricks2.innerHTML = '<strong>Mute</strong>'
+
+  var grabTricksBotoesImg2 = window.document.getElementById('grabTricksBotoesImg2')
+  grabTricksBotoesImg2.innerHTML = "<img src='./imagesArrows/seta_diagonal_cima_direita.png' width='25'>";
+  grabTricksBotoesImg2.innerHTML += "<img src='./imagesBotoesPsOne/botaoBolinha.png' width='25'>";
+
+  /* Grab Tricks 3 */
+  var nomeGrabTricks3 = window.document.getElementById('nomeGrabTricks3')
+  nomeGrabTricks3.innerHTML = '<strong>Indy</strong>'
+
+  var grabTricksBotoesImg3 = window.document.getElementById('grabTricksBotoesImg3')
+  grabTricksBotoesImg3.innerHTML = "<img src='./imagesArrows/seta_direita_laranja.png' width='25'>";
+  grabTricksBotoesImg3.innerHTML += "<img src='./imagesBotoesPsOne/botaoBolinha.png' width='25'>";
+
+  /* Grab Tricks 4 */
+  var nomeGrabTricks4 = window.document.getElementById('nomeGrabTricks4')
+  nomeGrabTricks4.innerHTML = '<strong>RoastBeef</strong>'
+
+  var grabTricksBotoesImg4 = window.document.getElementById('grabTricksBotoesImg4')
+  grabTricksBotoesImg4.innerHTML = "<img src='./imagesArrows/seta_diagonal_baixo_direita.png' width='25'>";
+  grabTricksBotoesImg4.innerHTML += "<img src='./imagesBotoesPsOne/botaoBolinha.png' width='25'>";
+
+  /* Grab Tricks 5 */
+  var nomeGrabTricks5 = window.document.getElementById('nomeGrabTricks5')
+  nomeGrabTricks5.innerHTML = '<strong>TailGrab</strong>'
+
+  var grabTricksBotoesImg5 = window.document.getElementById('grabTricksBotoesImg5')
+  grabTricksBotoesImg5.innerHTML = "<img src='./imagesArrows/seta_baixo_laranja.png' width='25'>";
+  grabTricksBotoesImg5.innerHTML += "<img src='./imagesBotoesPsOne/botaoBolinha.png' width='25'>";
+
+  /* Grab Tricks 6 */
+  var nomeGrabTricks6 = window.document.getElementById('nomeGrabTricks6')
+  nomeGrabTricks6.innerHTML = '<strong>Benihana</strong>'
+
+  var grabTricksBotoesImg6 = window.document.getElementById('grabTricksBotoesImg6')
+  grabTricksBotoesImg6.innerHTML = "<img src='./imagesArrows/seta_diagonal_baixo_esquerda.png' width='25'>";
+  grabTricksBotoesImg6.innerHTML += "<img src='./imagesBotoesPsOne/botaoBolinha.png' width='25'>";
+
+  /* Grab Tricks 7 */
+  var nomeGrabTricks7 = window.document.getElementById('nomeGrabTricks7')
+  nomeGrabTricks7.innerHTML = '<strong>Melon</strong>'
+
+  var grabTricksBotoesImg7 = window.document.getElementById('grabTricksBotoesImg7')
+  grabTricksBotoesImg7.innerHTML = "<img src='./imagesArrows/seta_esquerda_laranja.png' width='25'>";
+  grabTricksBotoesImg7.innerHTML += "<img src='./imagesBotoesPsOne/botaoBolinha.png' width='25'>";
+
+  /* Grab Tricks 8 */
+  var nomeGrabTricks8 = window.document.getElementById('nomeGrabTricks8')
+  nomeGrabTricks8.innerHTML = '<strong>CrossBone</strong>'
+
+  var grabTricksBotoesImg8 = window.document.getElementById('grabTricksBotoesImg8')
+  grabTricksBotoesImg8.innerHTML = "<img src='./imagesArrows/seta_diagonal_cima_esquerda.png' width='25'>";
+  grabTricksBotoesImg8.innerHTML += "<img src='./imagesBotoesPsOne/botaoBolinha.png' width='25'>";
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+  /* Lip Tricks */
+  /* Título Lip Tricks*/
+  var lipTricks = window.document.getElementById('lipTricks')
+  lipTricks.innerHTML = 'Lip Tricks<br>'
+
+  /* Lip Tricks 1 */
+  var nomeLipTricks = window.document.getElementById('nomeLipTricks')
+  nomeLipTricks.innerHTML = '<strong>Nosestall</strong>'
+
+  var lipTricksBotoesImg = window.document.getElementById('lipTricksBotoesImg')
+  lipTricksBotoesImg.innerHTML = "<img src='./imagesArrows/seta_cima_laranja.png' width='25'>";
+  lipTricksBotoesImg.innerHTML += "<img src='./imagesBotoesPsOne/botaoTriangulo.png' width='25'>";
+
+  /* Lip Tricks 2 */
+  var nomeLipTricks2 = window.document.getElementById('nomeLipTricks2')
+  nomeLipTricks2.innerHTML = '<strong>Axle Stall</strong>'
+
+  var lipTricksBotoesImg2 = window.document.getElementById('lipTricksBotoesImg2')
+  lipTricksBotoesImg2.innerHTML = "<img src='./imagesArrows/seta_direita_laranja.png' width='25'>";
+  lipTricksBotoesImg2.innerHTML += "<img src='./imagesBotoesPsOne/botaoTriangulo.png' width='25'>";
+
+  /* Lip Tricks 3 */
+  var nomeLipTricks3 = window.document.getElementById('nomeLipTricks3')
+  nomeLipTricks3.innerHTML = '<strong>Rock N Roll</strong>'
+
+  var lipTricksBotoesImg3 = window.document.getElementById('lipTricksBotoesImg3')
+  lipTricksBotoesImg3.innerHTML = "<img src='./imagesArrows/seta_baixo_laranja.png' width='25'>";
+  lipTricksBotoesImg3.innerHTML += "<img src='./imagesBotoesPsOne/botaoTriangulo.png' width='25'>";
+
+  /* Lip Tricks 4 */
+  var nomeLipTricks4 = window.document.getElementById('nomeLipTricks4')
+  nomeLipTricks4.innerHTML = '<strong>Disaster</strong>'
+
+  var lipTricksBotoesImg4 = window.document.getElementById('lipTricksBotoesImg4')
+  lipTricksBotoesImg4.innerHTML = "<img src='./imagesArrows/seta_esquerda_laranja.png' width='25'>";
+  lipTricksBotoesImg4.innerHTML += "<img src='./imagesBotoesPsOne/botaoTriangulo.png' width='25'>";
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+  /* Título Especiais*/
+  var especiais = window.document.getElementById('especiais')
+  especiais.innerHTML = 'Especiais<br>'
+
+  /* Especial 1 */
+  var nomeEspecias = window.document.getElementById('nomeEspecias')
+  nomeEspecias.innerHTML = '<strong>Nosegrind to Pivot</strong>'
+
+  var especiaisBotoesImg = window.document.getElementById('especiaisBotoesImg')
+  especiaisBotoesImg.innerHTML = "<img src='./imagesArrows/seta_baixo_laranja.png' width='25'>";
+  especiaisBotoesImg.innerHTML += "<img src='./imagesArrows/seta_cima_laranja.png' width='25'>";
+  especiaisBotoesImg.innerHTML += "<img src='./imagesBotoesPsOne/botaoTriangulo.png' width='25'>";
+
+  /* Especial 2 */
+  var nomeEspecias2 = window.document.getElementById('nomeEspecias2')
+  nomeEspecias2.innerHTML = '<strong>Chetto Bird</strong>'
+
+  var especiaisBotoesImg2 = window.document.getElementById('especiaisBotoesImg2')
+  especiaisBotoesImg2.innerHTML = "<img src='./imagesArrows/seta_baixo_laranja.png' width='25'>";
+  especiaisBotoesImg2.innerHTML += "<img src='./imagesArrows/seta_cima_laranja.png' width='25'>";
+  especiaisBotoesImg2.innerHTML += "<img src='./imagesBotoesPsOne/botaoQuadrado.png' width='25'>";
+
+  /* Especial 3 */
+  var nomeEspecias3 = window.document.getElementById('nomeEspecias3')
+  nomeEspecias3.innerHTML = '<strong>Casper</strong>'
+
+  var especiaisBotoesImg3 = window.document.getElementById('especiaisBotoesImg3')
+  especiaisBotoesImg3.innerHTML = "<img src='./imagesArrows/seta_esquerda_laranja.png' width='25'>";
+  especiaisBotoesImg3.innerHTML += "<img src='./imagesArrows/seta_baixo_laranja.png' width='25'>";
+  especiaisBotoesImg3.innerHTML += "<img src='./imagesBotoesPsOne/botaoBolinha.png' width='25'>";
+
+  boxInformacoesPersonagens.style.background = '#333'
+  boxInformacoesPersonagens.style.padding = '5px'
+
+  var up = window.document.getElementById('up')
+  up.innerHTML = 'Up'
+
+}
